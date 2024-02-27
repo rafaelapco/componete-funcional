caeee421a05b9da4506aaa105ddfc2f7a8dff215
Rafaela <rafaela.pacheco01@gmail.com>
2024-02-27 20:13:24 -0300

    first commit

---
+++ ComponenteFuncional/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Frase from './components/Frase'
+
+function App() {
+
+  return (
+    <>
+      <Frase name="Rafaela" age="25" />
+    </>
+  )
+}
+
+export default App
---
+++ ComponenteFuncional/src/components/Frase.jsx
@@ -0,0 +1,12 @@
+function Frase(props) {
+    return (
+      <div>
+        <h1>
+          Olá meu nome é {props.name}, tenho {props.age} anos
+        </h1>
+      </div>
+    );
+  }
+  
+  export default Frase;
+  
